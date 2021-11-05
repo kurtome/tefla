84e9ada5aaa35bfe81cdeb4959f641450310cc50
Kurtis Melby <kmelby@mbp14inch2021.myfiosgateway.com>
2021-11-04 23:47:22 -0400

    cleaning up CSS

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     plugins: [
+        require('postcss-import'),
+        require('tailwindcss/nesting')(require('postcss-nesting')),
         require('tailwindcss'),
-        require('autoprefixer'),
+        require('postcss-preset-env')({
+            features: { 'nesting-rules': false }
+        }),
     ]
 }
