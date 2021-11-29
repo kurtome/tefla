c8a0ef119cd323c04f460ec7dddb9f3e7646f356
Kurtis Melby <kurt@melby.me>
2021-11-28 23:34:08 -0500

    fixing deck images, using resizable svgs

--- lib/tefla_web/views/deck_helpers.ex
+++ lib/tefla_web/views/deck_helpers.ex
@@ -6,67 +6,32 @@ defmodule TeflaWeb.DeckHelpers do
   use Phoenix.HTML
   alias Tefla.Table.Card
 
-  # Values from sprite sheet specs
-  @height_px 450
-  @width_px 318
-  @row_gap_px 30
-  @col_gap_px 25
-  @suit_row %{
-    diamonds: 0,
-    clubs: 1,
-    hearts: 2,
-    spades: 3
-  }
-  @face_col %{
-    two: 0,
-    three: 1,
-    four: 2,
-    five: 3,
-    six: 4,
-    seven: 5,
-    eight: 6,
-    nine: 7,
-    ten: 8,
-    jack: 9,
-    queen: 10,
-    king: 11,
-    ace: 12
-  }
+  def card_back() do
+    content_tag(
+      :div,
+      tag(:img,
+        alt: "face down card",
+        src: "https://cdn.trick.games/cards_default/back.svg",
+        class: "w-full"
+      ),
+      class: "w-48"
+    )
+  end
 
   @doc """
   Generates a card element.
   """
   def card_img(%Card{} = card) do
-    card_img_row_col(@suit_row[card.suit], @face_col[card.face],
-      alt: "#{card.face} of #{card.suit}"
-    )
-  end
-
-  def card_back() do
-    card_img_row_col(2, 13, alt: "Card")
-  end
-
-  defp card_img_row_col(row, col, opts) do
-    alt = Keyword.get(opts, :alt, nil)
+    alt = "#{card.face} of #{card.suit}"
 
     content_tag(
       :div,
       tag(:img,
         alt: alt,
-        src: "https://cdn.trick.games/card_sprites.png",
-        style:
-          "height: #{@height_px}px; width: #{@width_px}px; object-fit: none; object-position: #{sprite_left(col)}px #{sprite_top(row)}px;"
+        src: "https://cdn.trick.games/cards_default/#{card.face}_of_#{card.suit}.svg",
+        class: "w-full"
       ),
-      class: "bg-white rounded border border-black",
-      style: "height: #{@height_px}px; width: #{@width_px}px;"
+      class: "w-48"
     )
-  end
-
-  defp sprite_top(row) do
-    -(row * @height_px + row * @row_gap_px)
-  end
-
-  defp sprite_left(col) do
-    -(col * @width_px + col * @col_gap_px)
   end
 end
